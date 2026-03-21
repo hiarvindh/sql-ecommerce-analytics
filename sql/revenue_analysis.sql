8dd7b294a66e7c3e16bf271821dc84cdcc4f5244
Arvindh Velrajan <tun67213@temple.edu>
2026-03-21 15:53:04 +0900

    Implement query for Late Delivery Rate
    Implement a query that can calculate the percentage of delivered orders that ended up arriving late.

--- sql/revenue_analysis.sql
+++ sql/revenue_analysis.sql
@@ -73,7 +73,14 @@ WHERE order_status = 'delivered'AND order_delivered_customer_date IS NOT NULL AN
 -- =========================================
 -- Question:
 -- What percentage of delivered orders arrived late?
-
+SELECT ROUND(100.0 * SUM(
+			CASE
+                WHEN order_delivered_customer_date > order_estimated_delivery_date THEN 1
+                ELSE 0
+            END
+	) / COUNT(*), 2) AS late_delivery_rate_pct
+FROM olist_orders_dataset
+WHERE order_status = 'delivered' AND order_delivered_customer_date IS NOT NULL AND order_estimated_delivery_date IS NOT NULL;
 
 -- =========================================
 -- 8. Delivery Time by State

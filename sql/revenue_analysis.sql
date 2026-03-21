c1ef876629b1b1162a72f78e4fa28777ce9d3d43
Arvindh Velrajan <tun67213@temple.edu>
2026-03-21 15:54:56 +0900

    Implement Delivery Time by State
    Use an SQL query with JOIN, GROUP BY, and ORDER BY to find out the states with the longest average delivery times.

--- sql/revenue_analysis.sql
+++ sql/revenue_analysis.sql
@@ -87,7 +87,13 @@ WHERE order_status = 'delivered' AND order_delivered_customer_date IS NOT NULL A
 -- =========================================
 -- Question:
 -- Which customer states have the longest average delivery times?
-
+SELECT c.customer_state, ROUND(AVG(DATEDIFF(o.order_delivered_customer_date, o.order_purchase_timestamp)), 2) AS avg_delivery_days
+FROM olist_orders_dataset o
+JOIN olist_customers_dataset c
+ON o.customer_id = c.customer_id
+WHERE o.order_status = 'delivered' AND o.order_delivered_customer_date IS NOT NULL AND o.order_purchase_timestamp IS NOT NULL
+GROUP BY c.customer_state
+ORDER BY avg_delivery_days DESC;
 
 -- =========================================
 -- 9. Review Score Distribution

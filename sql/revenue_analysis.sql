1d819c99cfd075755c629e829923bd77f8d31387
Arvindh Velrajan <tun67213@temple.edu>
2026-03-21 15:58:14 +0900

    Implement query for question 10
    Implement query to discover if late deliveries worse review scores or not.

--- sql/revenue_analysis.sql
+++ sql/revenue_analysis.sql
@@ -110,3 +110,13 @@ ORDER BY review_score;
 -- =========================================
 -- Question:
 -- Do late deliveries receive worse review scores?
+SELECT CASE
+        WHEN o.order_delivered_customer_date > o.order_estimated_delivery_date THEN 'Late'
+        ELSE 'On Time'
+    END AS delivery_status,
+ROUND(AVG(r.review_score), 2) AS avg_review_score, COUNT(*) AS total_reviews
+FROM olist_orders_dataset o
+JOIN olist_order_reviews_dataset r
+ON o.order_id = r.order_id
+WHERE o.order_status = 'delivered' AND o.order_delivered_customer_date IS NOT NULL AND o.order_estimated_delivery_date IS NOT NULL
+GROUP BY delivery_status;

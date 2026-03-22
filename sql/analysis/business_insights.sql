98b990addacce536e6f57664134680fb4afc4060
Arvindh Velrajan <tun67213@temple.edu>
2026-03-22 13:07:56 +0900

    Implement query
    Implement query to find the revenue and review score.

--- sql/analysis/business_insights.sql
+++ sql/analysis/business_insights.sql
@@ -17,3 +17,17 @@ SELECT
         2
     ) AS growth_pct
 FROM monthly_revenue;
+
+-- Revenue + Review Score
+SELECT COALESCE(pct.product_category_name_english, 'Unknown') AS category, ROUND(SUM(oi.price + oi.freight_value), 2) AS revenue, ROUND(AVG(r.review_score), 2) AS avg_review
+FROM olist_order_items_dataset oi
+JOIN olist_products_dataset p
+ON oi.product_id = p.product_id
+LEFT JOIN product_category_name_translation pct
+ON p.product_category_name = pct.product_category_name
+JOIN olist_orders_dataset o
+ON oi.order_id = o.order_id
+JOIN olist_order_reviews_dataset r
+ON o.order_id = r.order_id
+GROUP BY category
+ORDER BY revenue DESC;

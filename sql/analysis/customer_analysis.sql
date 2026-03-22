1f17ecfc62b7de41d85b3db19083f67522cb347a
Arvindh Velrajan <tun67213@temple.edu>
2026-03-22 13:00:37 +0900

    Implement query
    Implement query to find the "Repeat Customer" Rate.

--- sql/analysis/customer_analysis.sql
+++ sql/analysis/customer_analysis.sql
@@ -10,7 +10,17 @@ FROM (
 ) t;
 
 -- 2. Repeat Customer Rate
-
+SELECT ROUND(100.0 * SUM(CASE 
+	WHEN order_count > 1 THEN 1 
+    ELSE 0 END) / COUNT(*), 2) 
+AS repeat_customer_rate_pct
+FROM (
+    SELECT c.customer_unique_id, COUNT(DISTINCT o.order_id) AS order_count
+    FROM olist_customers_dataset c
+    JOIN olist_orders_dataset o
+	ON c.customer_id = o.customer_id
+    GROUP BY c.customer_unique_id
+) t;
 
 -- 3. Customer Lifetime Value (Top Customers)
 

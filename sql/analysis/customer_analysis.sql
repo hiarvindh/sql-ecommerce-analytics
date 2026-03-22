fb06192cbf173c5106cefd43f2ef9086a24533d3
Arvindh Velrajan <tun67213@temple.edu>
2026-03-22 12:58:59 +0900

    Implement Average Order Value (AOV)
    Implement code to find the average order value.

--- sql/analysis/customer_analysis.sql
+++ sql/analysis/customer_analysis.sql
@@ -1,6 +1,13 @@
 USE ecommerce_analytics_olist;
 -- 1. Average Order Value (AOV)
-
+SELECT ROUND(AVG(order_value), 2) AS avg_order_value
+FROM (
+    SELECT o.order_id, SUM(oi.price + oi.freight_value) AS order_value
+    FROM olist_orders_dataset o
+    JOIN olist_order_items_dataset oi
+        ON o.order_id = oi.order_id
+    GROUP BY o.order_id
+) t;
 
 -- 2. Repeat Customer Rate
 
